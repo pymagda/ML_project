5e288cb3f976afe3189cc618d9a65d5ddaca4a99
Magdalena Pęksa <magdapeksa@gmail.com>
2017-06-12 22:45:06 +0200

    Make color enhacement more generic, add color enum

--- classification/color_enhacement.py
+++ classification/color_enhacement.py
@@ -2,14 +2,18 @@ import cv2 as cv
 import numpy as np
 
 
+class ColorBGR:
+    blue, green, red = range(3)
+
+
 class ColorEnhancement:
 
     def __init__(self, img):
         self.img = img
 
-    def process_frame(self):
-        (channel_b, channel_g, channel_r) = cv.split(self.img)
-        s = np.array(channel_b.astype(int) + channel_g.astype(int) + channel_r.astype(int)) + np.finfo(float).eps
-        v = np.dstack((channel_b.astype(int)-channel_g.astype(int), channel_b.astype(int)-channel_r.astype(int))).min(axis=2)
+    def process_frame(self, color):
+        channels = np.array(cv.split(self.img)).astype(int)
+        s = np.array(np.sum(channels, axis=0) + np.finfo(float).eps)
+        v = np.dstack((channels[color]-channels[(color+1) % 3], channels[color]-channels[(color+2) % 3])).min(axis=2)
         output = np.dstack((v/s, np.zeros(v.shape))).max(axis=2)
         return output
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from classification.color_enhacement import ColorEnhancement
+from classification.color_enhacement import ColorEnhancement, ColorBGR
 import cv2 as cv
-import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     img = cv.imread('data/znak_nakazu.jpg')
     c = ColorEnhancement(img)
-    img2 = c.process_frame()
+    img2 = c.process_frame(ColorBGR.blue)
     cv.namedWindow('image', cv.WINDOW_NORMAL)
     cv.imshow('image', img2)
     cv.waitKey(0)
