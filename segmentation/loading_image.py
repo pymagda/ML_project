d268b52eb02c679fac8f4a0994c43a42913d01c4
Magdalena Pęksa <magdapeksa@gmail.com>
2017-06-18 22:30:07 +0200

    Cosmetics

--- segmentation/loading_image.py
+++ segmentation/loading_image.py
@@ -21,28 +21,3 @@ class Load_image:
 
     def getFrame(self):
         return cv2.flip(self.currentFrame, 0)
-
-
-
-
-
-    """def __init__(self, ip_address):
-        self.cap = cv2.VideoCapture(ip_address)
-
-    def load_image(self):
-
-        i = 0
-        while True:
-            ret, frame = self.cap.read()
-            ret, frame = self.cap.read()
-            ret, frame = self.cap.read()
-            ret, frame = self.cap.read()
-            ret, frame = self.cap.read()
-            ret, frame = self.cap.read()
-            frame = cv2.flip(frame, 0)
-           # cv2.imshow('frame', frame)
-            if cv2.waitKey(30) == 27:
-                break
-            yield frame
-        self.cap.release()
-        cv2.destroyAllWindows()"""
